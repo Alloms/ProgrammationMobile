b9bead5531d17e91a68d6157bafa12fb2538323e
Matteo Quintero <quintero@et.esiea.fr>
2020-08-27 22:28:55 +0200

    Ajout Serialisation Ajout Interface avec API Ajout API Crypto

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.myapplication;
 
+
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        showList();
+        makeApicall();
+
+
+    }
+
+    private static final String BASE_URL = "https://api.coinlore.net/";
+
+
+    private void showList() {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
@@ -44,6 +57,44 @@ public class MainActivity extends AppCompatActivity {
         // define an adapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+    }
 
+
+    private void makeApicall() {
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+
+        Call<RestAPIRep> call = cryptoAPI.getCryptoRep();
+        call.enqueue(new Callback<RestAPIRep>() {
+
+            @Override
+            public void onResponse(Call<RestAPIRep> call, Response<RestAPIRep> response) {
+                if (response.isSuccessful() && response.body() != null){
+                    List<Crypto> CryptoList = response.body().getData();
+                    Toast.makeText(getApplicationContext(),"API Success", Toast.LENGTH_SHORT).show();
+                } else{
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestAPIRep> call, Throwable t) {
+                showError();
+            }
+        });
+
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
